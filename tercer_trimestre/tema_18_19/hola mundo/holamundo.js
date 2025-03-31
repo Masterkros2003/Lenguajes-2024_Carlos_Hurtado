5cfd158e187102f16ee56b3114f05eb51ab31d9f
Carlitos <chc0011@alu.medac.es>
2025-03-31 16:12:51 +0200

    commit

---
+++ tercer_trimestre/tema_18_19/hola mundo/holamundo.js
@@ -0,0 +1,7 @@
+function miFuncion() {
+    var  resultado = document.getElementById('hola');  
+    resultado.innerText = "Hola desde un elemento obtenido por su ID";
+    alert("Hola desde una alerta");
+    console.log("Hola desde la consola");
+  }
+  
